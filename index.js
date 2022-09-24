87ada7a51dcee086b9594a2974f878de050877d5
Jordan Yamada <sadaotomokane@gmail.com>
2022-09-24 00:02:33 +0000

    fixed nodemon issue

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 require('dotenv').config();
-const { server } = require('./src/server.js');
-const { db } = require('./src/models');
+const server = require('./src/server.js');
+const { db } = require('./src/models/index.js');
 const PORT = process.env.PORT || 3001;
 
 db.sync()
